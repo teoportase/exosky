368f5090b5f4f6b711a3bafdb2aa1c4cd46f2bee
Marcelo <marsant@student.chalmers.se>
2024-10-06 11:57:09 +0200

    Info about the planet when hovering, removed img randomization

--- src/app/components/Planet.js
+++ src/app/components/Planet.js
@@ -1,5 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
+
+import PlanetInfo from "./PlanetInfo";
 
 import planet1 from "@/app/images/venus-transparent.png";
 import planet2 from "@/app/images/planet.png";
@@ -8,31 +12,28 @@ import planet4 from "@/app/images/mercury-transparent.png";
 
 //maybe make this automatic for planets we have
 
-function Planet({ count }) {
-  count = 8; //how many planets to load
-  const planetImages = [planet1, planet2, planet3, planet4]; //array with all the planet images
-
-  //function to choose a random planets
+function Planet() {
+  const [isShown, setIsShown] = useState(false);
+  console.log(isShown);
 
   return (
-    <div className="grid lg:grid-flow-col lg:auto-cols-max grid-cols-2 gap-16">
-      {Array.from({ length: count }).map((_, index) => {
-        // Pick a random planet for each div rendering
-        const randomPlanet =
-          planetImages[Math.floor(Math.random() * planetImages.length)];
-
-        return (
-          <div className="size-32" key={index}>
-            <a href="/" className="">
-              <Image
-                src={randomPlanet}
-                alt={`planetIcon-${index}`}
-                className="transition-all duration-300 hover:scale-125"
-              />
-            </a>
-          </div>
-        );
-      })}
+    <div>
+      <div className="grid lg:grid-flow-col lg:auto-cols-max grid-cols-2 gap-12">
+        <div className="size-32">
+          <a href="/" className="">
+            <Image
+              src={planet1}
+              alt="planet"
+              className="transition-all duration-300 hover:scale-125"
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
+            />
+          </a>
+        </div>
+      </div>
+      <div className={isShown ? "visible" : "hidden"}>
+        <PlanetInfo />
+      </div>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Planet from "@/app/components/Planet.js";
 
 export default function Home() {
+  const numberOfPlanets = 8;
+
   return (
     <div className="bg-black h-screen w-full overflow-hidden">
       <div className="flex justify-center lg:mt-80 mt-24">
-        <Planet />
+        {Array.from({ length: numberOfPlanets }).map((_, index) => (
+          <Planet key={index} />
+        ))}
       </div>
     </div>
   );
